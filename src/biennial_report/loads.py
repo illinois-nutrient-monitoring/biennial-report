4c2c0dd017af3d1192c28cbf2c75539fa1853cbd
thodson <thodson@usgs.gov>
2022-08-26 15:34:11 -0500

    Working on revisions

--- src/biennial_report/loads.py
+++ src/biennial_report/loads.py
@@ -3,72 +3,36 @@ import xarray as xr
 def location_label(site):
     return site['river'] + ' ' + site['location']
 
-#def compute_river_load(ds, site, nested=True):
-#    
-#    site_load = ds.sel(site_no=site['gage_id'])
-#    site_load = site_load.drop_vars('site_no')
-#    site_load = site_load * site['scale_factor']
-#
-#    upstream_gage = site.get('upstream_gage')
-#    
-#    if upstream_gage and nested:
-#        upstream_load = ds.sel(site_no=upstream_gage).drop_vars('site_no')
-#        site_load = site_load - upstream_load
-#        
-#    site_load = site_load.assign_coords(coords={'river':location_label(site)})
-#    
-#    return site_load
 
-def compute_river_load(ds, site, nested=True):
-    
+def compute_river_load(ds, site, subtract_nested=True):
+
     site_load = ds.sel(site=site['gage_id'])
     site_load = site_load.drop_vars('site')
 
     upstream_gage = site.get('upstream_gage')
     
-    if upstream_gage and nested:
+    # if nesting enabled, subtract upstream load
+    if subtract_nested and upstream_gage:
         upstream_load = ds.sel(site=upstream_gage).drop_vars('site')
         site_load = site_load - upstream_load
-        
+
     site_load = site_load.assign_coords(coords={'river':location_label(site)})
-    
+
     site_load = site_load * site['scale_factor']
     return site_load
 
 
-
-
-def compute_network_loads(ds, network, nested=True):
+def compute_network_loads(ds, network, subtract_nested=True):
     rivers = []
-    
+
     for site in network:
         if site['gage_id'] in ds.site:
-            rivers.append(compute_river_load(ds, site, nested))
-        
-    return xr.concat(rivers, dim='river')
-    #return rivers
+            nested = site.get('nested')
+            if not nested or subtract_nested is False:
+                rivers.append(compute_river_load(ds, site, subtract_nested))
 
-#@def labels(network):
-#@    labels = []
-#@    for site in network:
-#@        nested = site.get('nested')
-#@
-#@        if nested is None or nested==False:
-#@            #print(nested)
-#@            labels.append(location_label(site))
-#@            
-#@    return labels
-#@
-#@def gages(network):
-#@    labels = []
-#@    for site in network:
-#@        nested = site.get('nested')
-#@
-#@        if nested is None or nested==False:
-#@            #print(nested)
-#@            labels.append(site['gage_id'])
-#@            
-#@    return labels
+    return xr.concat(rivers, dim='river')
+
 
 def kg_to_lbs(x):
     return x * 2.20462
@@ -76,24 +40,29 @@ def kg_to_lbs(x):
 def kg_to_kt(x):
     return x * 1.0e-6
 
-def labels(network, nested=False):
+def labels(network, include_nested=False):
     labels = []
     for site in network:
         is_nested = site.get('nested')
-
-        if is_nested is None or is_nested==nested:
-            #print(nested)
+        
+        if is_nested is None:
             labels.append(location_label(site))
-            
+        elif include_nested and is_nested is True:
+            labels.append(location_label(site))
+
+#        if is_nested is None or is_nested==nested:
+#            labels.append(location_label(site))
+
     return labels
 
-def gages(network, nested=False):
+def gages(network, include_nested=False):
     labels = []
     for site in network:
         is_nested = site.get('nested')
 
-        if is_nested is None or is_nested==nested:
-            #print(nested)
+        if is_nested is None:
             labels.append(site['gage_id'])
-            
+        elif include_nested and is_nested is True:
+            labels.append(site['gage_id'])
+
     return labels
--- src/biennial_report/plot.py
+++ src/biennial_report/plot.py
@@ -4,11 +4,14 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
+import matplotlib
 
-def percentage_scale(baseline, ax):
+def percentage_scale(baseline, ax, diff=False):
     lim = ax.get_ylim()
     ax2 = ax.twinx()
-    delta = (lim - baseline)/ baseline * 100
+    delta = (lim - baseline)/baseline * 100
+    if diff:
+        delta = delta + 100
     ax2.set_ylabel('[percentage]')
     ax2.set_ylim(delta)
     return ax2
@@ -16,42 +19,42 @@ def percentage_scale(baseline, ax):
 def annotate_percentage_difference(recent_mean, baseline_mean, loc, ax):
     if loc is None:
         loc='upper right'
-        
+
     delta = (recent_mean - baseline_mean)/ baseline_mean * 100
     annotate_text = "Change: {:.1f}%".format(delta.values)
     #at = AnchoredText(annotate_text, prop=dict(size=10), frameon=False, loc=loc)
     at = AnchoredText(annotate_text, frameon=False, loc=loc)
     ax.add_artist(at)
-    
- 
- 
+
+
+
 def print_period(years):
     return f'{years[0]}-{years[-1]}'
 
 def title_axes(ax):
     ax.set_xlabel( ax.get_xlabel().title())
     ax.set_ylabel( ax.get_ylabel().title())
-    
-    
+
+
 def annotate_difference(delta, loc, ax):
     if loc is None:
         loc='upper right'
-        
+
     #delta = (recent_mean - baseline_mean)/ baseline_mean * 100
     annotate_text = "Change: {:.1f}%".format(delta.values)
     #at = AnchoredText(annotate_text, prop=dict(size=10), frameon=False, loc=loc)
     at = AnchoredText(annotate_text, frameon=False, loc=loc)
     ax.add_artist(at)
-    
-    
+
+
 def mean_and_se(ds, dim):
     mean = ds.mean(dim=dim)
     se = ds.std(dim=dim) / np.sqrt(ds.sizes[dim])
     return mean, se
 
-    
+
 def plot_mean_error(ds, period, color, ax, linewidth=2, extend=None):
-    # ds.sel(river=labels(network1)).sum(dim='river').sel(year=ds.year.dt.year.isin(period))    
+    # ds.sel(river=labels(network2)).sum(dim='river').sel(year=ds.year.dt.year.isin(period))
     mean, se = mean_and_se(ds, dim='year')
 
     xmin = pd.to_datetime(str(period[0]))
@@ -59,21 +62,21 @@ def plot_mean_error(ds, period, color, ax, linewidth=2, extend=None):
     ax.hlines(y=mean, xmin=xmin, xmax=xmax, linewidth=linewidth, color=color, label=print_period(period))
     ax.hlines(y=mean+1.96*se, xmin=xmin, xmax=xmax, linewidth=1, ls='-', color=color)
     ax.hlines(y=mean-1.96*se, xmin=xmin, xmax=xmax, linewidth=1, ls='-', color=color)
-    
+
     if extend:
-        extend =  pd.to_datetime(str(extend))
+        extend = pd.to_datetime(str(extend))
         ax.hlines(y=mean, xmin=xmax, xmax=extend, linewidth=linewidth, ls='dotted', color=color)
 
 
 def running_average_plot(ds1,
                          period1,
                          period2,
-                         ds2=None, 
+                         ds2=None,
                          ax=None,
                          loc=None):
     if ax is None:
         fig, ax = plt.subplots()
-    
+
     p1_ds = ds1.sel(year=ds1.year.dt.year.isin(period1))
     p1_mean = p1_ds.mean()
 
@@ -81,40 +84,39 @@ def running_average_plot(ds1,
         temp = ds1
     else:
         temp = ds2
-        
+
     p2_ds = temp.sel(year=temp.year.dt.year.isin(period2))
     p2_mean = p2_ds.mean()
-   
+
+    
     plot_mean_error(ds=p1_ds,
                     period=period1,
                     color='red',
                     extend=period2[0],
                     ax=ax)
-    
-    
+
+
     plot_mean_error(ds=p2_ds,
                     period=period2,
                     color='black',
                     ax=ax)
 
-    ds1.plot(marker='o', color='k', 
-             linestyle='None', 
-             markeredgecolor='k', 
-             markerfacecolor='None', 
+    ds1.plot(marker='o', color='k',
+             linestyle='None',
+             markeredgecolor='k',
+             markerfacecolor='None',
              ax=ax,
              label=ds1.attrs.get('label'))
-    
-    if ds2 is not None: 
-        ds2.plot(marker='o', color='k', 
-                 linestyle='None', 
-                 markeredgecolor='k', 
-                 markerfacecolor='k', 
+
+    if ds2 is not None:
+        ds2.plot(marker='o', color='k',
+                 linestyle='None',
+                 markeredgecolor='k',
+                 markerfacecolor='k',
                  ax=ax,
                  label=ds2.attrs.get('label'))
-
-    #annotate_difference(p1_mean, p2_mean, loc, ax)
+    
     annotate_percentage_difference(p2_mean, p1_mean, loc, ax)
-    #annotate_difference(p2_mean, loc, ax)
     title_axes(ax)
 
 
@@ -123,64 +125,17 @@ def append_total(ds, label='Statewide'):
     return xr.concat([total, ds], dim='river')
 
 
-
-
-
-#def plot_change_by_basin(period1_ds, period2_ds, ax=None, color='k', statewide=False):
-#    '''
-#    ds1 = ambient_loads[parameter].sel(year=ambient_loads.year.dt.year.isin(baseline_years)).mean(dim='year')
-#    ds2 = supergage_loads[parameter].sel(year=supergage_loads.year.dt.year.isin(study_years)).mean(dim='year')
-#    '''
-#    if ax is None:
-#        fig, ax = plt.subplots()
-#        
-#    # compute standard error of baseline
-#    #(p1-p2) / p3
-#    # first compute error in p1-p2
-#    div = period1_ds.mean(dim='year')
-#    if statewide:
-#        div = div.sum()
-#        
-#    # append total
-#    period1_ds = append_total(period1_ds)
-#    period2_ds = append_total(period2_ds)
-#    
-#    if not statewide:
-#        div = period1_ds.mean(dim='year')
-#
-#    mean1, se1 = mean_and_se(period1_ds, dim='year')
-#    mean2, se2 = mean_and_se(period2_ds, dim='year')#ds.mean()
-#        
-#    difference = (mean2 - mean1).to_series()
-#    difference_se = np.sqrt(se1**2 + se2**2).to_series()
-#
-#        
-#    difference = difference/div.values * 100
-#    difference_se = difference_se/div.values * 100
-#    
-#    # compute percent contribution to statewide change
-#    #if statewide:
-#    #    difference /= difference['Statewide'] * 0.01
-#    #    difference_se /= difference_se['Statewide'] * 0.01
-#
-#    #title = period1_ds.name.title()
-#    
-#    difference.plot.bar(ax=ax, yerr=difference_se*1.96, color=color, edgecolor='k', capsize=4)
-#    
-#    #ax.set_ylabel(f'{title} [{units}]')
-#    ax.set_xlabel('')
-#    ax.axhline(y=0, lw=1, color='k')
-
 def plot_by_basin(period_ds, ax=None, color='k'):
     if ax is None:
         fig, ax = plt.subplots()
- 
+
     mean, se = mean_and_se(period_ds, dim='year')
     mean = mean.to_series()
     se = se.to_series()
     mean.plot.bar(ax=ax, yerr=se*1.96, color=color, edgecolor='k', capsize=4)
+
     
-    
+
 def plot_change_by_basin(period1_ds, period2_ds,
                          ax=None, color='k',
                          mode='load', da=None):
@@ -190,34 +145,45 @@ def plot_change_by_basin(period1_ds, period2_ds,
     '''
     if ax is None:
         fig, ax = plt.subplots()
-        
+
     title = period1_ds.name
-    
+
     # append total
     period1_ds = append_total(period1_ds, mode=mode, da=da)
     period2_ds = append_total(period2_ds, mode=mode, da=da)
-    
+
     mean1, se1 = mean_and_se(period1_ds, dim='year')
-    mean2, se2 = mean_and_se(period2_ds, dim='year')#ds.mean()
-        
+    mean2, se2 = mean_and_se(period2_ds, dim='year')
+
     difference = (mean2 - mean1).to_series()
     difference_se = np.sqrt(se1**2 + se2**2).to_series()
-   
-   
+
+
     difference.plot.bar(ax=ax, yerr=difference_se*1.96, color=color, edgecolor='k', capsize=4)
-    
+
     units = period1_ds.pint.units
     ax.set_ylabel(f'{title} [{units}]'.capitalize())
     ax.set_xlabel('')
     ax.axhline(y=0, lw=1, color='k')
+    #format_axes(ax)
 
+    
 def append_total(ds, label='Statewide', mode='load', da=None):
     if mode=='load':
         total = ds.sum(dim='river')
-    
-    elif mode=='yield':
+
+    elif mode == 'yield':
         total = ds.sum(dim='river')/da.sum()
         ds = ds/da
-    
+
     return xr.concat([total.assign_coords({"river":label}), ds], dim='river')
 
+def format_yaxis(ax):
+    ax.get_yaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ','))) 
+
+def format_xaxis(ax):
+    ax.get_xaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ','))) 
+#current_streamflow = mean_flow.sel(year=mean_flow.year.dt.year.isin(study_years))
+#current_streamflow = mean_flow.sel(year=mean_flow.year.dt.year.isin(study_years))
